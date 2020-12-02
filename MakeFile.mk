ea731f2c6c18517460de5f96df3063ce5440b585
EnvyDya <bonduelleclment@gmail.com>
2020-12-02 15:31:45 +0100

    merge coolos

--- MakeFile.mk
+++ MakeFile.mk
@@ -1,5 +1,5 @@
 BIN=bin/TachiAi.exe
-OBJECTS=src/main.o src/bouge.o
+OBJECTS=src/main.o src/bouge.o src/affichage.o src/alea.o
 CC=gcc
 CFLAGS=-I include -L lib -lmingw32 -lSDL2main -lSDL2
 
@@ -12,5 +12,10 @@ main.o: main.c
 bouge.o: bouge.c
 	$(CC) -c src/bouge.c $(CFLAGS)
 
+affichage.o: affichage.c
+	$(CC) -c src/affichage.c $(CFLAGS)
+
+alea.o: alea.c
+	$(CC) -c src/alea.c $(CFLAGS)
 clear:
 	del bin\*.exe src\*.o
