e0c8c528b235b26b4fb734a1f626fdd39e351f68
Siegroud <siegrid@outlook.fr>
2020-12-09 14:43:20 +0100

    Ajout makefile sprite

---
+++ MakeFile.mk
@@ -0,0 +1,24 @@
+BIN=bin/TachiAi.exe
+OBJECTS=src/main.o src/bouge.o src/affichage.o src/alea.o
+CC=gcc
+CFLAGS=-I include -L lib -lmingw32 -lSDL2main -lSDL2
+
+all: $(OBJECTS)
+	$(CC) -o $(BIN) $(OBJECTS) $(CFLAGS)
+
+main.o: main.c
+	$(CC) -c src/main.c $(CFLAGS)
+
+bouge.o: bouge.c
+	$(CC) -c src/bouge.c $(CFLAGS)
+
+affichage.o: affichage.c
+	$(CC) -c src/affichage.c $(CFLAGS)
+
+alea.o: alea.c
+	$(CC) -c src/alea.c $(CFLAGS)
+clear:
+	del bin\*.exe src\*.o
+
+clearO:
+	del src\*.o
